e4621bb7f132f5fff8ef30a0f09276545e3ce648
Hashib Raja <hashibraja23@gmail.com>
2023-08-05 14:02:31 +0530

    Writing firs code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("first")
+import { OpenAI } from "langchain/llms/openai";
+
+const llm = new OpenAI({
+  openAIApiKey: "YOUR_KEY_HERE",
+  temperature: 0.9,
+});
+
+const result = await llm.predict(
+  "What would be a good company name for a company that makes colorful socks?"
+);
+// "Feetful of Fun"
---
+++ src/langchainLLM.d.ts
@@ -0,0 +1,1 @@
+declare module "langchain/llms/openai";
